426f28a508f18baf9e02c2bd5360bc25119ac238
Om <om.rawal@somaiya.edu>
2022-07-08 16:09:20 +0530

    Added calculator without testing

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator{
+	public static void main(String[] args) {
+		System.out.println(add(10,20)+" is the result of adding 10,20");
+		System.out.println(subtract(10,20)+" is the result of subtracting 10,20");
+	}
+	public int add(int a,int b) {
+		System.out.println("In Addition");
+		return a+b;
+	}
+	public int subtract(int a,int b) {
+		System.out.println("In Subtraction");
+		return a-b;
+	}
+}
